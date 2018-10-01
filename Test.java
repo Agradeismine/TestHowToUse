12880031b93ed5d3d9697b16596401303ac97dc4
Agradeismine <39588951+Agradeismine@users.noreply.github.com>
2018-10-01 12:57:19 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test{
 	public static void main (String [] args){
 		
 		System.out.println("Don't watch my shit program...plz!");
-		
+		System.out.println("If you watch it, you will be shit too...");		
 	}
 	
 }
